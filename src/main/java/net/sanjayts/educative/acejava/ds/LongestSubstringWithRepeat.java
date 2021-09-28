bafd8859741d4093528dfa5cc143aa408385efa7
Sanjay <30881+sanjayts@users.noreply.github.com>
2021-09-28 21:50:45 +0100

    solution for permutations and longest substring without repeat

---
+++ src/main/java/net/sanjayts/educative/acejava/ds/LongestSubstringWithRepeat.java
@@ -0,0 +1,34 @@
+package net.sanjayts.educative.acejava.ds;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+public class LongestSubstringWithRepeat {
+
+    public static void main(String[] args) {
+        LongestSubstringWithRepeat prg = new LongestSubstringWithRepeat();
+        String s = "abcdabcd";
+        int len = prg.lengthOfLongestSubstring(s);
+        log.info("string={}; longest substring={}", s, len);
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        Integer[] seen = new Integer[256];
+        int len = 0, left = 0, right = 0;
+        while (right < s.length()) {
+            char ch = s.charAt(right);
+            if (seen[ch] != null && seen[ch] >= left) {
+                left = seen[ch] + 1;
+            }
+            seen[ch] = right;
+            len = Math.max(len, right - left + 1);
+            right++;
+        }
+        return len;
+    }
+
+}
---
+++ src/main/java/net/sanjayts/educative/acejava/ds/Permutations.java
@@ -0,0 +1,44 @@
+package net.sanjayts.educative.acejava.ds;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Slf4j
+public class Permutations {
+
+    public static void main(String[] args) {
+        var arr = new int[]{1, 2, 3};
+        var out = new Permutations().permute(arr);
+        log.info("Permutations of array {} are {}", arr, out);
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new LinkedList<>();
+        boolean[] visited = new boolean[nums.length];
+        dfs(nums, res, new LinkedList<>(), visited, 0);
+        return res;
+    }
+
+    private void dfs(int[] nums, List<List<Integer>> res, List<Integer> curr, boolean[] visited, int level) {
+        log.info("Invoked DFS with cur={}, level={}; visited={}", curr, level, visited);
+        if (curr.size() == nums.length) {
+            res.add(new LinkedList<>(curr));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            visited[i] = true;
+            curr.add(nums[i]);
+            dfs(nums, res, curr, visited, level + 1);
+            Integer item = curr.get(curr.size() - 1);
+            curr.remove(curr.size() - 1);
+            visited[i] = false;
+            log.info("Removed from curr, item={}, level={}, visited={}", item, level, visited);
+        }
+    }
+
+}
